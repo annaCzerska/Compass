fddfc22012439cff28afa170af8e57883858db0e
Anna Czerska <ania.czerska002@gmail.com>
2023-02-17 14:09:05 +0100

    Add compass animation and degree converter
    Added RotateAnimation - rotate animation of dial_compass and n_sign to show direction
    
    Added method degreeConverter - direction converter from degree to name of cardinal direction

--- app/src/main/java/com/example/compass/MainActivity.java
+++ app/src/main/java/com/example/compass/MainActivity.java
@@ -7,22 +7,28 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    ImageView dial_compass;
+    ImageView dial_compass, n_sign;
     TextView direction_txt;
 
     private SensorManager mSensorManager;
     Sensor accelerometer;
     Sensor magnetometer;
+    float azimuth = 0f;
+    float azimuth_current = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dial_compass = findViewById(R.id.dial_compass);
+        n_sign = findViewById(R.id.n_sign);
         direction_txt = findViewById(R.id.direction_txt);
 
         if (getSupportActionBar() != null) {
@@ -62,11 +68,47 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             if(success){
                 float orientation[] = new float[3];
                 SensorManager.getOrientation(R, orientation);
-                float azimuth = (float)Math.toDegrees(orientation[0]);
-                azimuth = (azimuth+360)%360;
-                int degree = (int)azimuth;
-                direction_txt.setText(Integer.toString(degree) + " degrees");
+                azimuth = (float)Math.toDegrees(orientation[0]);
+                float azimuth_round = (azimuth+360)%360;
+                int degree = (int)azimuth_round;
+                direction_txt.setText(Integer.toString(degree) + '\u00B0' + " " + degreeConverter(degree));
+
+                RotateAnimation animation = new RotateAnimation(azimuth_current,-azimuth_round,Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                RotateAnimation animation_N = new RotateAnimation(azimuth_current,-azimuth,Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF, 3.75f);
+                animation.setDuration(250);
+                animation.setRepeatCount(0);
+                animation.setFillAfter(true);
+
+                animation_N.setDuration(250);
+                animation_N.setRepeatCount(0);
+                animation_N.setFillAfter(true);
+
+                dial_compass.startAnimation(animation);
+                n_sign.startAnimation(animation_N);
+
+                azimuth_current = -azimuth;
             }
         }
+    }
+    public String degreeConverter(int windDegree){
+        String direction = "";
+        if(windDegree>=0 && windDegree<=23 || windDegree>=337 && windDegree<=360){
+            direction = " North";
+        }else if( windDegree>=24 && windDegree<=68){
+            direction = " Northeast";
+        }else if( windDegree>=69 && windDegree<=113){
+            direction = " East";
+        }else if( windDegree>=114 && windDegree<=158){
+            direction = " Southeast";
+        }else if( windDegree>=159 && windDegree<=203){
+            direction = " South";
+        }else if( windDegree>=204 && windDegree<=248){
+            direction = " Southwest";
+        }else if( windDegree>=249 && windDegree<=293){
+            direction = " West";
+        }else if( windDegree>=294 && windDegree<=336){
+            direction = " Northwest";
+        }
+        return direction;
     }
 }
